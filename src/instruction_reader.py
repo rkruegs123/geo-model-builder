51c9cda7d824917cf18db8767ae9a8884410159c
rkruegs123 <rkruegs123@gmail.com>
2020-09-27 14:04:23 +0100

    Progress

--- src/instruction_reader.py
+++ src/instruction_reader.py
@@ -1,10 +1,14 @@
 import pdb
 import argparse
+import collections
 
 from instruction import Assert, AssertNDG, Confirm, Sample, Parameterize, Compute
 from constraint import Constraint
 from parse import parse_sexprs
 from cline import Line, Circle
+from util import Root
+
+Point = collections.namedtuple("Point", ["val"])
 
 class InstructionReader:
     def __init__(self, lines):
@@ -24,13 +28,17 @@ class InstructionReader:
             self.add(cmd)
         elif pred == "compute":
             self.compute(cmd)
+        elif pred == "confirm":
+            self.confirm(cmd)
         else:
             pdb.set_trace()
             raise NotImplementedError(f"[InstructionReader.process_command] Command not supported: {pred}")
 
 
     def sample(self, cmd):
-        ps = cmd[1]
+        ps = [self.process_term(p) for p in cmd[1]]
+        assert(all([isinstance(p.val, str) for p in ps]))
+
         method = cmd[2]
         if len(cmd) > 3:
             args = cmd[3]
@@ -47,42 +55,86 @@ class InstructionReader:
         constraint = cmd[1][1] if negate else cmd[1]
         cons_pred = constraint[0]
         cons_args = constraint[1:]
-        if all([isinstance(arg, str) for arg in cons_args]):
-            instr_cons = Constraint(cons_pred, cons_args, False)
-            if negate:
-                self.instructions.append(AssertNDG(instr_cons))
-            else:
-                self.instructions.append(Assert(instr_cons))
-        else:
-            # Note that when we do, we should be keeping track of the points
-            pdb.set_trace()
-            raise NotImplementedError(f"[add] Do not yet support non-point arguments")
+        cons_args = [self.process_term(t) for t in cons_args]
 
+        instr_cons = Constraint(cons_pred, cons_args, False)
+        if negate:
+            self.instructions.append(AssertNDG(instr_cons))
+        else:
+            self.instructions.append(Assert(instr_cons))
+
+
+    # TODO: All this should move to process_point, and we should check that the value is NOT a string!
     def compute(self, cmd):
         if len(cmd) != 3:
             raise RuntimeError(f"Malformed compute command: {cmd}")
 
         p = cmd[1]
-        c_method = cmd[2][0]
-        c_args = cmd[2][1:]
+        computation = self.process_point(cmd[2])
+        c_instr = Compute(p, computation)
+        self.instructions.append(c_instr)
 
-        if c_method == "incenter":
-            assert(len(c_args) == 3)
-            assert(all([isinstance(arg, str) for arg in c_args]))
-            c_instr = Compute(p, ("incenter", c_args))
-            self.instructions.append(c_instr)
-        elif c_method == "interLL":
-            l1 = self.get_line(c_args[0])
-            l2 = self.get_line(c_args[1])
-            c_instr = Compute(p, ("interLL", l1, l2))
-            self.instructions.append(c_instr)
+    def confirm(self, cmd):
+        negate = (cmd[1][0] == "not")
+        constraint = cmd[1][1] if negate else cmd[1]
+        cons_pred = constraint[0]
+        cons_args = constraint[1:]
+        cons_args = [self.process_term(t) for t in cons_args]
+
+        instr_cons = Constraint(cons_pred, cons_args, negate)
+        self.instructions.append(Confirm(instr_cons))
+
+    def process_term(self, term):
+        try:
+            return self.process_point(term)
+        except:
+            try:
+                return self.process_line(term)
+            except:
+                try:
+                    return self.process_circle(term)
+                except:
+                    raise RuntimeError(f"Term {term} not a point/line/circle")
+
+    def process_point(self, p_info):
+        if isinstance(p_info, str):
+            return Point(p_info)
+        if not isinstance(p_info, tuple):
+            raise NotImplementedError(f"[process_point] p_info must be tuple or string")
+
+        p_pred = p_info[0]
+        p_args = p_info[1:]
+        if p_pred == "interLL":
+            assert(len(p_args) == 2)
+            l1 = self.process_line(p_args[0])
+            l2 = self.process_line(p_args[1])
+            p_val = ("interLL", l1, l2)
+            return Point(p_val)
+        elif p_pred == "incenter":
+            assert(len(p_args) == 3)
+            ps = [self.process_point(p) for p in p_args]
+            p_val = ("incenter", ps)
+            return Point(p_val)
+        elif p_pred == "interLC":
+            assert(len(p_args) == 3)
+            l = self.process_line(p_args[0])
+            c = self.process_circle(p_args[1])
+            rs = self.process_rs(p_args[2])
+            p_val = ("interLC", l, c, rs)
+            return Point(p_val)
+        elif p_pred == "interCC":
+            assert(len(p_args) == 3)
+            c1 = self.process_circle(p_args[0])
+            c2 = self.process_circle(p_args[1])
+            rs = self.process_rs(p_args[2])
+            p_val = ("interCC", c1, c2, rs)
+            return Point(p_val)
         else:
-            pdb.set_trace()
-            raise NotImplementedError(f"[compute] Do not yet support method {c_method}")
+            raise NotImplementedError(f"[process_point] Unrecognized p_pred {p_pred}")
 
-    def get_line(self, l_info):
+    def process_line(self, l_info):
         l_pred = l_info[0]
-        ps = l_info[1:]
+        ps = [self.process_point(p) for p in l_info[1:]]
         if l_pred == "line":
             assert(len(ps) == 2)
             return Line("connecting", ps)
@@ -90,7 +142,29 @@ class InstructionReader:
             assert(len(ps) == 3)
             return Line("perpAt", ps)
         else:
-            return NotImplementedError(f"[get_line] Unsupported line pred: {l_pred}")
+            pdb.set_trace()
+            return NotImplementedError(f"[process_line] Unsupported line pred: {l_pred}")
+
+    def process_circle(self, c_info):
+        c_pred = c_info[0]
+        ps = [self.process_point(p) for p in c_info[1:]]
+        if c_pred == "circ":
+            assert(len(ps) == 3)
+            return Circle("c3", ps)
+        else:
+            pdb.set_trace()
+            return NotImplementedError(f"[process_circle] Unsupported circle pred: {c_pred}")
+
+    def process_rs(self, rs_info):
+        rs_pred = rs_info[0]
+        rs_args = rs_info[1:]
+        if rs_pred == "rsNeq":
+            assert(len(rs_args) == 1)
+            p_neq = self.process_point(rs_info[0])
+            return Root("neq", [p_neq])
+        else:
+            pdb.set_trace()
+            return NotImplementedError(f"[process_rs] Unsupported rs pred: {rs_pred}")
 
 if __name__ == "__main__":
     # Get problem to compile
